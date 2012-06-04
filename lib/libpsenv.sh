a4fc5259eb01513a5b3c16752f16d6b538419d6a
JR Bing <jrbing@gmail.com>
2012-06-04 13:58:27 -0400

    Added help documentation

--- lib/libpsenv.sh
+++ lib/libpsenv.sh
@@ -1,10 +1,43 @@
 #!/usr/bin/env bash
 # Library file for psenv
 
+####################
+# Help Documentation
+####################
+
+print_help () {
+# Prints the help documentation
+cat <<- EOF
+
+  # PSENV #
+
+  Description:
+    psenv is a ...
+
+  Commands:
+    ...         ...
+
+EOF
+}
+
+###########
+# Utilities
+###########
+
+log () {
+  if [[ $VERBOSE ]]; then
+    printf "[PSENV]: $1\n" >&2
+  fi
+}
+
+############
+# Validation
+############
+
 contains_environment () {
   local n=$#
   local value=${!n}
-  for ((i=1;i < $#;i++)) {
+  for ((i=1; i < $#; i++)) {
       if [ "${!i}" == "${value}" ]; then
           echo "y"
           return 0
@@ -12,12 +45,6 @@ contains_environment () {
   }
   echo "n"
   return 1
-}
-
-log () {
-  if [[ $VERBOSE ]]; then
-    printf "[PSENV]: $1\n" >&2
-  fi
 }
 
 load_environment_list () {
@@ -36,21 +63,15 @@ check_for_environment_dir () {
   fi
 }
 
-source_env_file () {
-  log "Sourcing the environment file..."
-  . $PS_ENV_HOME/$1.psenv
-}
+#############################
+# Reset environment variables
+#############################
 
 environment_reset () {
   log "Resetting environment variables..."
   unset IS_PS_PLT
   unset PATH
   unset LD_LIBRARY_PATH
-}
-
-source_psconfig () {
-  log "Sourcing the psconfig.sh file...."
-  . $PS_HOME/psconfig.sh # Source psconfig.sh
 }
 
 restore_library_path () {
@@ -63,20 +84,34 @@ restore_path () {
   export PATH=$ORIGINAL_PATH
 }
 
+##############################
+# Update environment variables
+##############################
+
+source_env_file () {
+  log "Sourcing the environment file..."
+  . $PS_ENV_HOME/$1.psenv
+}
+
+source_psconfig () {
+  log "Sourcing the psconfig.sh file..."
+  . $PS_HOME/psconfig.sh # Source psconfig.sh
+}
+
 set_library_path () {
   log "Updating LD_LIBRARY_PATH..."
   export LD_LIBRARY_PATH=$TUXDIR/lib:$LD_LIBRARY_PATH
-  export LD_LIBRARY_PATH=$JAVA_HOME/lib:$LD_LIBRARY_PATH
-  export LD_LIBRARY_PATH=$COBDIR/lib:$LD_LIBRARY_PATH
-  export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
+  [[ $JAVA_HOME ]] && export LD_LIBRARY_PATH=$JAVA_HOME/lib:$LD_LIBRARY_PATH
+  [[ $COBDIR ]] && export LD_LIBRARY_PATH=$COBDIR/lib:$LD_LIBRARY_PATH
+  [[ $ORACLE_HOME ]] && export LD_LIBRARY_PATH=$ORACLE_HOME/lib:$LD_LIBRARY_PATH
 }
 
 set_path () {
   log "Updating PATH..."
   export PATH=$PATH:.
   export PATH=$TUXDIR/bin:$PATH
-  export PATH=$COBDIR/bin:$PATH
-  export PATH=$ORACLE_HOME/bin:$PATH
-  export PATH=$AGENT_HOME/bin:$PATH
+  [[ $COBDIR ]] && export PATH=$COBDIR/bin:$PATH
+  [[ $ORACLE_HOME ]] && export PATH=$ORACLE_HOME/bin:$PATH
+  [[ $AGENT_HOME ]] && export PATH=$AGENT_HOME/bin:$PATH
 }
 
