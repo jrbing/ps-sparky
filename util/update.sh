74d776c7562292e9760119442fcd03e1e405b0e3
JR Bing <jrbing@gmail.com>
2012-06-07 08:42:19 -0400

    Cleanup comments

--- util/update.sh
+++ util/update.sh
@@ -3,7 +3,6 @@
 ##############################
 # Fix for invalid certificates
 ##############################
-
 # Remove the temporary curl cert bundle if it exists
 if [[ -f /tmp/curl-ca-bundle-new.crt  ]]; then
   rm /tmp/curl-ca-bundle-new.crt
@@ -13,6 +12,9 @@ fi
 /usr/bin/env curl -sSfL https://www.digicert.com/testroot/DigiCertHighAssuranceEVRootCA.crt >> /tmp/curl-ca-bundle-new.crt
 /usr/bin/env curl -sSfL https://www.digicert.com/CACerts/DigiCertHighAssuranceEVCA-1.crt >> /tmp/curl-ca-bundle-new.crt
 
+#############################
+# Pull down files from github
+#############################
 printf "Updating ps-sparky from github:\n"
 cd ~ && \
 /usr/bin/env rm -rf /tmp/ps-sparky* && \
@@ -21,5 +23,8 @@ cd ~ && \
 /usr/bin/env tar -xvf /tmp/ps-sparky.tar && \
 /usr/bin/env cp -rf jrbing-ps-sparky-???????/* .ps-sparky/
 
+########################
+# Remove temporary files
+########################
 printf "Cleaning up...\n"
 /usr/bin/env rm -rf jrbing-ps-sparky-???????/
