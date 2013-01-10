84a21f5afa8e966360efc1fbddcf9a306141921a
JR Bing <jr@jrbing.com>
2013-01-10 10:24:16 -0500

    Outlined functionality

--- lib/libpscfg.sh
+++ lib/libpscfg.sh
@@ -12,25 +12,115 @@ cat <<- EOF
   # PSCFG #
 
   Description:
-    pscfg is a utility script that acts as a cli front end for configuration
-    executables and files
+    pscfg is a utility script that acts as a cli front end for Sparky
+    configuration files
 
   Commands:
+    list        List environment configuration files
+    show        Show environment information
+    create      Create environment configuration file
+    edit        Edit specified environment configuration file
+    delete      Delete specified environment configuration file
+    copy        Copies the specified environment configuration file
+    toggle      Toggle specified environment setting
     help        Displays the help menu
 
 EOF
 }
 
-# Prints the help documentation for the "status" command
+# Prints the help documentation for the "edit" command
 printEditHelp () {
 cat <<- EOF
 
   Usage:
-  psadm status [ app web prcs agent all ]
+  pscfg edit [ app web prcs agent all ]
 
   Description:
-  Shows the status of the server process specified in the argument
+  Opens the specified configuration file in the default editor
 
 EOF
 }
 
+# Prints the help documentation for the "show" command
+printShowHelp () {
+cat <<- EOF
+
+  Usage:
+  psadm show [ vars ]
+
+  Description:
+  Returns environment information for the argument specified
+
+EOF
+}
+
+#########
+# Utility
+#########
+
+log () {
+  printf "\n\e[00;31m[PSCFG]: $1\e[00m\n" >&2
+}
+
+######
+# List
+######
+
+listEnvironments () {
+  echo "Environment listings..."
+}
+
+######
+# Show
+######
+
+# Displays PeopleSoft-specific environment variables
+showPsftVars () {
+  for i in $ENV_VARS; do
+    printf $i is set to `printenv $i`
+  done
+}
+
+########
+# Create
+########
+
+createEnvironment () {
+  echo "Creating environment file"
+}
+
+######
+# Edit
+######
+
+editEnvironment () {
+  echo "Editing environment file"
+}
+
+
+########
+# Delete
+########
+
+deleteEnvironment () {
+  echo "Deleting environment file"
+}
+
+######
+# Copy
+######
+
+copyEnvironment () {
+  echo "Copying environment file"
+}
+
+########
+# Toggle
+########
+
+########
+# Doctor
+########
+runHealthCheck () {
+  echo "Running doctor..."
+}
