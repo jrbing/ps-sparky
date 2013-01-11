9d7741d7be736dadacce7af051107fc36dbdf5a4
JR Bing <jr@jrbing.com>
2013-01-11 13:59:08 -0500

    Better spinner

--- lib/libutil.sh
+++ lib/libutil.sh
@@ -120,17 +120,78 @@ multiTail () {
   wait
 }
 
-spinner () {
-  local pid=$1
-  local delay=0.75
-  local spinstr='|/-\'
-  while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-    local temp=${spinstr#?}
-    printf " [%c]  " "$spinstr"
-    local spinstr=$temp${spinstr%"$temp"}
-    sleep $delay
-    printf "\b\b\b\b\b\b"
-  done
-  printf "    \b\b\b\b"
+function _spinner() {
+
+  # $1 start/stop
+  #
+  # on start: $2 display message
+  # on stop : $2 process exit status
+  #           $3 spinner function pid (supplied from stop_spinner)
+
+  local on_success="DONE"
+  local on_fail="FAIL"
+  local white="\e[1;37m"
+  local green="\e[1;32m"
+  local red="\e[1;31m"
+  local nc="\e[0m"
+
+  case $1 in
+
+    (start)
+      # Calculate the column where spinner and status msg will be displayed
+      let column=$(tput cols)-${#2}-8
+      # Display message and position the cursor in $column column
+      echo -ne ${2}
+      printf "%${column}s"
+
+      # Start spinner
+      i=1
+      sp='\|/-'
+      delay=0.15
+
+      while :
+      do
+        printf "\b${sp:i++%${#sp}:1}"
+        sleep $delay
+      done
+      ;;
+
+    (stop)
+      if [[ -z ${3} ]]; then
+        printf "Spinner is not running.."
+        exit 1
+      fi
+
+      kill $3 > /dev/null 2>&1
+
+      # Inform the user upon success or failure
+      echo -en "\b["
+      if [[ $2 -eq 0 ]]; then
+        echo -en "${green}${on_success}${nc}"
+      else
+        echo -en "${red}${on_fail}${nc}"
+      fi
+      echo -e "]"
+      ;;
+
+    (*)
+      printf "Invalid argument, try {start/stop}\n"
+      exit 1
+      ;;
+
+  esac
 }
 
+function start_spinner {
+  # $1 : msg to display
+  _spinner "start" "${1}" &
+  # set global spinner pid
+  _sp_pid=$!
+  disown
+}
+
+function stop_spinner {
+  # $1 : command exit status
+  _spinner "stop" $1 $_sp_pid
+  unset _sp_pid
+}
